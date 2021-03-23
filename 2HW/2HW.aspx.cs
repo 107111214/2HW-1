b9221e4c33dc855bd50b865f598f5075f4353ea2
思甯 王 <107111214@mail.oit.edu.tw>
2021-03-23 16:02:42 +0800

    作業2

---
+++ 2HW/2HW.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class _2HW : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 6;
+            int sum = 0;
+            for (int i = 0; i <= N; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Response.Write(sum);
+        }
+    }
+}
